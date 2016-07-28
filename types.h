3c053b763bca443f223696b22a4178b98c784706
Zateryukin <igor.zateryukin@hpe.com>
2016-07-28 18:45:16 +0300

    first commit

---
+++ types.h
@@ -0,0 +1,17 @@
+#ifndef __types_h__
+#define __types_h__
+#include <sys/types.h>
+
+typedef int8_t byte;
+typedef uint8_t ubyte;
+typedef uint16_t uword;
+typedef uint32_t udword;
+typedef int16_t word;
+typedef int32_t dword;
+typedef unsigned char bool;
+
+#define TRUE 1
+#define FALSE 0
+
+#endif
+
